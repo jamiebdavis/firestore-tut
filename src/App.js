49ceb413af35e41aac3ec4021b08ba251d72911a
Jamie Davis <jamie.davis@ao.com>
2020-08-26 18:42:16 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import React, { useEffect, useState } from "react";
+
+import firebase from "./firebase";
+import { useForm } from "react-hook-form";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [cafes, setCafes] = useState([]);
+    const [db, setDb] = useState(firebase.firestore());
+
+    const { register, handleSubmit, watch, errors, reset } = useForm();
+
+    useEffect(() => {
+        // const db = firebase.firestore();
+        // const data = await db.collection("cafes").where("city", "==", "Manchester").orderBy("name").get();
+        // const data = await db.collection("cafes").orderBy("name").get();
+
+        // Returns the result of on snapshot call
+        return db
+            .collection("cafes")
+            .orderBy("name")
+            .onSnapshot(snapshot => {
+                // let changes = snapshot.docChanges();
+                let changes = [];
+
+                snapshot.forEach(doc => changes.push({ ...doc.data(), id: doc.id }));
+                setCafes(changes);
+            });
+    }, []);
+
+    const onSubmit = async data => {
+        const res = await db.collection("cafes").add({
+            name: data.name,
+            city: data.city,
+        });
+
+        console.log("Added document with ID: ", res.id);
+
+        reset();
+    };
+
+    const handleDelete = async id => {
+        const res = await db.collection("cafes").doc(id).delete();
+        console.log(id, " deleted");
+    };
+
+    return (
+        <div>
+            <h1>Cloud Cafe</h1>
+
+            <div className="content">
+                {/* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    {/* register your input into the hook by invoking the "register" function */}
+                    <input name="name" placeholder="name" ref={register} />
+
+                    {/* include validation with required or other standard HTML validation rules */}
+                    <input name="city" placeholder="city" ref={register} />
+
+                    <input type="submit" />
+                </form>
+
+                <ul id="cafe-list">
+                    {cafes.map(doc => {
+                        return (
+                            <li key={doc.id}>
+                                <span>{doc.name}</span>
+                                <span>{doc.city}</span>
+                                <div onClick={() => handleDelete(doc.id)}>x</div>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Your web app's Firebase configuration
+
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDfzC8pP9JQCzVlWMl1JSF0Usy7LHeop28",
+    authDomain: "firestore-tut-713b2.firebaseapp.com",
+    databaseURL: "https://firestore-tut-713b2.firebaseio.com",
+    projectId: "firestore-tut-713b2",
+    storageBucket: "firestore-tut-713b2.appspot.com",
+    messagingSenderId: "798138100440",
+    appId: "1:798138100440:web:a18c00e8a1a2a860e305ad",
+    measurementId: "G-R6XMH5L2WB",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
